d20c08bb0b0858e2c8d452cd9972dc778b69e304
MartinaBasile <martinaleandribasile@gmail.com>
2023-01-24 16:19:13 +0100

    Add files via upload
    Classe con metodo print() di una variabile,
    Definizione di vari tipi di variabili

---
+++ HelloBonus.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+public class HelloBonus {
+
+	public static void main(String[] args) {
+		// dichiarazione e assegnazione variabili
+		String name= "Martina";
+		String surname;
+		surname="Basile";
+		
+		System.out.println("Benvenuta " + name + " " + surname);
+		
+		//altri tipi di variabili 
+		// ""-> indica una stringa ''-> inidica un char
+		double altezza=1.60;
+		int eta=30;
+		boolean automunita=true;
+		char sesso='F'; 
+	}
+
+}
